84a159366424ed6aaadab516b7e633cb390a82d3
David Fong <dav770@outlook.com>
2019-10-18 14:07:55 -0400

    Ex 5: modified the directional key to have more predator class

--- exercises/exercise5/js/Predator.js
+++ exercises/exercise5/js/Predator.js
@@ -12,7 +12,7 @@ class Predator {
   //
   // Sets the initial values for the Predator's properties
   // Either sets default values or uses the arguments provided
-  constructor(x, y, speed, fillColor, radius) {
+  constructor(x, y, speed, fillColor, radius, upKey, downKey, leftKey, rightKey) {
     // Position
     this.x = x;
     this.y = y;
@@ -28,11 +28,13 @@ class Predator {
     // Display properties
     this.fillColor = fillColor;
     this.radius = this.health; // Radius is defined in terms of health
+
     // Input properties
-    this.upKey = UP_ARROW;
-    this.downKey = DOWN_ARROW;
-    this.leftKey = LEFT_ARROW;
-    this.rightKey = RIGHT_ARROW;
+    // Modified the argument of the arrow keys
+    this.upKey = upKey;
+    this.downKey = downKey;
+    this.leftKey = leftKey;
+    this.rightKey = rightKey;
   }
 /////////////////////////// End New ///////////////////////////////////////////
   // handleInput
