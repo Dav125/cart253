cebfcdd61cad0c6499c4f12963453493b54589b8
David Fong <dav770@outlook.com>
2019-10-22 17:26:41 -0400

    Ex 5: Turned the program as an Array
    - Erase the old variables of the prey
    - Added new variables of the prey to make the array work
    - added for loop
    -added push() to add new preys
    - erased the prey. move() , handleEating and display() for new ones
    -Added another for the new to be kept track of being able move and eaten by the predator. hanbleEating

--- exercises/exercise5/js/script.js
+++ exercises/exercise5/js/script.js
@@ -15,10 +15,13 @@ let lion; // New
 
 /////////////////////////// End New //////////////////////////////////
 
-// The three prey
-let antelope;
-let zebra;
-let bee;
+// New Prey
+let newAntelope = [];
+let newZebra = [];
+let newBee = [];
+
+// How many preys that are being shown
+let numPreys = 1;
 
 //////////////////////////// Start New ////////////////////////////////
 
@@ -35,9 +38,30 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   tiger = new Predator(100, 100, 5, color(200, 200, 0), 40, UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW);
   lion = new Predator(200, 200, 5, color(0, 200, 200), 45, 87, 83, 65, 68); // New
-  antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
-  zebra = new Prey(100, 100, 8, color(255, 255, 255), 60);
-  bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
+
+  for (let i = 0; i < numPreys; i++) {
+
+    // random()
+    //
+    // Both the foodX and foodY are here
+    // to change the position of the new preys
+    let foodX = random(0, width);
+    let foodY = random(0, height);
+
+    // New variable to be able
+    // to add new array version of the animals
+    let addAntelope = new Prey(foodX, foodY, 10, color(255, 100, 10), 50);
+    let addZebra = new Prey(foodX, foodY, 8, color(255, 255, 255), 60);
+    let addBee = new Prey(foodX, foodY, 20, color(255, 255, 0), 10);
+
+    // push()
+    //
+    // To add new preys in the contanner of the array
+    // so that new preys will appear
+    newAntelope.push(addAntelope);
+    newZebra.push(addZebra);
+    newBee.push(addBee);
+  }
 }
 
 //////////////////////////// End New ///////////////////////////////////
@@ -48,7 +72,7 @@ function setup() {
 function draw() {
   // Clear the background to black
   background(0);
-//////////////////////////// Start New /////////////////////////////////
+  //////////////////////////// Start New /////////////////////////////////
   // Handle input for the tiger
   tiger.handleInput();
 
@@ -58,25 +82,69 @@ function draw() {
   // Move all the "animals"
   tiger.move();
   lion.move(); // New
-  antelope.move();
-  zebra.move();
-  bee.move();
-
-  // Handle the tiger eating any of the prey
-  tiger.handleEating(antelope);
-  tiger.handleEating(zebra);
-  tiger.handleEating(bee);
-
-  // New handle for the lion to eat its prey
-  lion.handleEating(antelope); // New
-  lion.handleEating(zebra); // New
-  lion.handleEating(bee); // New
 
   // Display all the "animals"
   tiger.display();
   lion.display(); // New
-  antelope.display();
-  zebra.display();
-  bee.display();
-//////////////////////////// End New ////////////////////////////////////////
+
+  // For loop function
+  //
+  // New animals will appear as array objects
+
+  // For the new antelope that appear
+  for (let i = 0; i < newAntelope.length; i++) {
+
+    // All of the move() and display() of the new antelope
+    newAntelope[i].move();
+    newAntelope[i].display();
+
+    //handleEating()
+    //
+    // Both the tiger and lion will be able to eat the
+    // new array object version of the preys
+    //
+    // Also having the [i] with the new animals
+    // helps keep track of the new animals
+    tiger.handleEating(newAntelope[i]);
+    lion.handleEating(newAntelope[i]);
+  }
+
+  // For the new Zebra that will appear
+  for (let i = 0; i < newZebra.length; i++) {
+
+    // All of the move() and display() of the new Zebra
+    newZebra[i].move();
+    newZebra[i].display();
+
+
+    //handleEating()
+    //
+    // Both the tiger and lion will be able to eat the
+    // new array object version of the preys
+    //
+    // Also having the [i] with the new animals
+    // helps keep track of the new animals
+    tiger.handleEating(newZebra[i]);
+    lion.handleEating(newZebra[i]);
+  }
+
+  // For the new Bee that will appear
+  for (let i = 0; i < newBee.length; i++) {
+
+    // All of the move() and display() of the new Bee
+    newBee[i].move();
+    newBee[i].display();
+
+    //handleEating()
+    //
+    // Both the tiger and lion will be able to eat the
+    // new array object version of the preys
+    //
+    // Also having the [i] with the new animals
+    // helps keep track of the new animals
+    tiger.handleEating(newBee[i]);
+    lion.handleEating(newBee[i]);
+  }
+
+  //////////////////////////// End New ////////////////////////////////////////
 }
