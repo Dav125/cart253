37b9147e1f9ee4ee0025636ce5742099ce5aec4c
David Fong <dav770@outlook.com>
2019-10-25 15:56:22 -0400

    Ex 6: Fixed ellipse()
    Wrong use with "two", we just need the use of number 2

--- exercises/exercise6/js/Prey.js
+++ exercises/exercise6/js/Prey.js
@@ -96,7 +96,9 @@ class Prey {
       noStroke();
       fill(this.fillColor);
       this.radius = this.health;
-      ellipse(this.x, this.y, this.radius * "two");
+///////////////////////////////////////////////////////// FIXED
+      // ellipse(this.x, this.y, this.radius * "two"); =====> Wrong use with "two", we just need the use of number 2
+      ellipse(this.x, this.y, this.radius * 2);
       pop();
     }
 
