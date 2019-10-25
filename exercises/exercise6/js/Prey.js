5c402c5e233e5150d5de68e03a740f991b51fcec
David Fong <dav770@outlook.com>
2019-10-25 16:08:59 -0400

    Ex 6: if statement with this.x
    this.x is supposed to be less 0

--- exercises/exercise6/js/Prey.js
+++ exercises/exercise6/js/Prey.js
@@ -74,7 +74,9 @@ class Prey {
     // wraps it to the other side if so
     handleWrapping() {
       // Off the left or right
-      if (this.x > 0) {
+/////////////////////////////////////////////////////////// FIXED
+      // if (this.x > 0) { ===> this.x is supposed to be less 0
+      if (this.x < 0) {
         this.x += width;
       }
       else if (this.x > width) {
