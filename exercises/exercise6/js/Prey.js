284eb1cb008dfefd3c71500eb54acc4761f13f42
David Fong <dav770@outlook.com>
2019-10-25 16:05:07 -0400

    Ex 6: Fixed noise() for ty
    Missing the max value to use noise(), so I added 1000 in the random()

--- exercises/exercise6/js/Prey.js
+++ exercises/exercise6/js/Prey.js
@@ -33,7 +33,9 @@ class Prey {
 ////////////////////////////////////////////////////////////// FIXED
     // this.tx = random(0, 0); ==> Missing max value to use noise()
     this.tx = random(0, 1000); // To make x and y noise different
-    this.ty = random(0, 0); // we use random starting values
+////////////////////////////////////////////////////////////// FIXED
+    // this.ty = random(0, 0); ==> Missing the max value to use noise()
+    this.ty = random(0, 1000); // we use random starting values
     // Health properties
     this.maxHealth = radius;
     this.health = this.maxHealth; // Must be AFTER defining this.maxHealth
