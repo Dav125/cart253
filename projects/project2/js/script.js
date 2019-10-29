37a7119f554d7a7a6d4953112d107c10bb71b53a
David Fong <dav770@outlook.com>
2019-10-29 17:32:30 -0400

    P2: Adding a gradient
    1. added startColor and endColor as a variable for making a gradient
    2. I've assign the variable with a color.
    3. I erased background() and replaced it with setGradient in draw() as a setup for a new function called setGradient
    4. In the setGradient(), I added a for loop with map() to create my gradient

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -1,5 +1,5 @@
 // Predator-Prey Simulation
-// by Pippin Barr
+// by David Fong
 //
 // Creates a predator and three prey (of different sizes and speeds)
 // The predator chases the prey using the arrow keys and consumes them.
@@ -13,6 +13,11 @@ let antelope;
 let zebra;
 let bee;
 
+// Adding these two variable to create a horizontal
+// gradient
+let startColor;
+let endColor;
+
 // setup()
 //
 // Sets up a canvas
@@ -23,6 +28,13 @@ function setup() {
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
   zebra = new Prey(100, 100, 8, color(255, 255, 255), 60);
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
+
+  // color()
+  //
+  // Setting these 2 set of color to make gradient
+  // it allows me to modify my color picker
+  startColor = color(255,0,255);
+  endColor = color(0);
 }
 
 // draw()
@@ -30,7 +42,11 @@ function setup() {
 // Handles input, movement, eating, and displaying for the system's objects
 function draw() {
   // Clear the background to black
-  background(0);
+  //background(0);
+
+  // Applying this code as a background color
+  // to make
+  setGradient(0,0,windowWidth, windowHeight, startColor, endColor);
 
   // Handle input for the tiger
   tiger.handleInput();
@@ -51,4 +67,35 @@ function draw() {
   antelope.display();
   zebra.display();
   bee.display();
+}
+
+// setGradient()
+//
+// Im using this function to set up the parameter
+// to create my gradient
+function setGradient(x, y, w, h, c1, c2){
+
+push();
+noFill();
+
+// Using for loop
+//
+// it allows me to create a starting point (cx)
+// it creates lines of color that changes
+// from the start to finish with window width
+for(let cx = x; cx <= x + w; cx++ ){
+
+  // map()
+  //
+  // the program allows me to calculate the ratio
+  // of the background by percentage decimal from
+  // left to right
+  let inter = map(cx, x, x + w, 0, 1);
+  let c = lerpColor(c1 ,c2 ,inter);
+  stroke(c);
+  line(cx, y, cx, y + h);
+}
+
+pop();
+
 }
