84c9859a61a579da0f12c579addf8a6e5a62ad01
David Fong <dav770@outlook.com>
2019-11-11 15:06:56 -0500

    Ex7: Initial commit

---
+++ exercises/exercise7/js/Climber.js
@@ -0,0 +1,46 @@
+// Climber class
+//
+// It is a class with the player controlling it
+
+class Climber { // A Climber class describes what a Predator is and does
+  constructor(x, y, speed, fillColor, radius) {
+    this.x = x;
+    this.y = y;
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = speed;
+    this.upKey = UP_ARROW;
+    this.downKey = DOWN_ARROW;
+    this.leftKey = LEFT_ARROW;
+    this.rightKey = RIGHT_ARROW;
+  }
+
+  //handleInput()
+  //
+  // The controls for the climber class
+  handleInput() {
+
+  }
+
+  // move()
+  //
+  //
+  move() {
+
+  }
+
+  // handleWrapping()
+  //
+  //
+  handleWrapping() {
+
+
+  }
+
+  //display()
+  //
+  //
+  display() {
+
+  }
+}
---
+++ exercises/exercise7/js/script.js
@@ -0,0 +1,35 @@
+// Exercise 7
+//
+// By David Fong
+//
+// In this exercise, it is going to be a portion of a platform game
+// when the player just to platform to platform to the top
+
+
+// preload()
+//
+// To load the background asset and the sound asset for the
+// for the mountain
+
+function preload() {
+  
+}
+
+
+// setup()
+//
+// To set-up the the background of the mountain
+
+function setup() {
+  createCanvas(1280, 720);
+
+}
+
+//draw()
+//
+// description of draw()
+
+function draw() {
+  background(0);
+
+}
