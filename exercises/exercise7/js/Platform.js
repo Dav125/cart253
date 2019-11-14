d5c96da9a47e5a6d7fc4da3a05b3118d8328dfa0
David Fong <dav770@outlook.com>
2019-11-13 20:37:01 -0500

    Ex 7: Added Platform class
    -added x,y for the positions
    -added scale to change size of the image
    - added variable for platform images
    - Added display()

---
+++ exercises/exercise7/js/Platform.js
@@ -0,0 +1,34 @@
+// Platform Class
+//
+// This is a class when all the platform are positioned
+
+ class Platform {
+   // constructor()
+   //
+   // This is all of the parametres for the platform to be placed
+   constructor(x, y, width, height, platformImg) {
+     // Postion
+     this.x = x;
+     this.y = y;
+     // Size
+     // Scale of the image
+     this.scale = 0.1;
+     this.width = width * this.scale;
+     this.height = height * this.scale;
+     // Variable to display the image
+     this.platformImg = platformImg;
+
+
+   }
+
+
+   // diplay();
+   //
+   // Display the platform in the background
+   display() {
+     push();
+     imageMode(CENTER);
+     image(this.platformImg,this.x, this.y, this.width, this.height);
+     pop();
+   }
+ }
