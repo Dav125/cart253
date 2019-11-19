c9f3ae838fee5b81082e4f0d222458693f111d17
David Fong <dav770@outlook.com>
2019-11-19 14:39:08 -0500

    Ex 8: Preparing variables and preload function
    - added climber variable for avatar
    - added mountainImage for background
    - added climberImg to load avatar img
    - added platShortImg to load short platform
    - added platLongImg to load long platform
    -added description for preload function
    - added loadImage functions for the assets

--- exercises/exercise8/js/script.js
+++ exercises/exercise8/js/script.js
@@ -7,15 +7,37 @@ Exercise 8
 David Fong
 
 In exercise 8, it is going to be an improved version of the
-exercise 7 with the platform climber to the top 
+exercise 7 with the platform climber to the top
 
 ******************/
 
+// Variable for the player avatar
+let climber;
+
+// Variable for the background image
+let mountainImage;
+
+// Variable for the number of platform
+let numPlat = 20;
+
+// Images asset for the game
+let climbImg;
+let platShortImg;
+let platLongImg;
+let mountainImg;
+
 // preload()
 //
-// Description of preload
+// To load the image assets for the game 
 
 function preload() {
+  // background()
+  //
+  // To load the image assets for the start of the game
+  climbImg = loadImage("assets/images/AvatarClimber.jpg");
+  platShortImg = loadImage("assets/images/platform.jpg");
+  platLongImg = loadImage("assets/images/platform2.jpg");
+  mountainImg = loadImage("assets/images/mountain.jpg");
 
 }
 
