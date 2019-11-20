8bb8a147bf77e724d8dc5d2e9c832dd14a0a84dc
David Fong <dav770@outlook.com>
2019-11-19 19:29:05 -0500

    Ex 8: Added for loop in setup
    - added random x and y repostioning for both platform
    -added short and long platform class
    - added push function

--- exercises/exercise8/js/script.js
+++ exercises/exercise8/js/script.js
@@ -58,7 +58,38 @@ createCanvas(1280, 720);
   // To put postion, the size, putting image and the speed
   climber = new Climber(500, 500, 500, 500, 2, climbImg, UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW);
 
-  
+
+  // For loop
+  //
+  // To make array objects for platform
+  for (let i = 0; i < numbPlat; i++) {
+
+    // random()
+    //
+    // adding position for the platforms
+
+    // For the position the short platform
+    let reShortX = random(0, width);
+    let reShortY = random(0, height);
+
+    // For the position for long platform
+    let reLongX = random(0, width);
+    let reLongY = random(0, width);
+
+    // Platform classes
+    //
+    // Platform that will be generated in the screen
+    platformShortPlus = new Platform(reShortX, reShortY, 500, 500, platShortImg);
+    platformLongPlus = new Platform(reLongX, reLongY, 1000, 500, platLongImg);
+
+    // push()
+    //
+    // To add more platform in the screen as an array
+    platformShort.push(platformShortPlus);
+    platformLong.push(platformLongPlus);
+
+  }
+
 
 }
 
